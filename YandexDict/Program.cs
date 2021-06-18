2db580d6b576d9dc380c170e05ede33f06ef7e6a
IlyaGavhsin <60525281+IlyaGavhsin@users.noreply.github.com>
2021-06-18 13:01:17 +0300

    Make all program all right

--- YandexDict/Program.cs
+++ YandexDict/Program.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using Newtonsoft.Json;
 using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 
 namespace YandexDict
 {
@@ -9,49 +11,67 @@ namespace YandexDict
     {
         static void Main(string[] args)
         {
+            TelegramBotClient bot = new TelegramBotClient("1848825408:AAHpxH4ZgBQFHItNUOIR5-vCIrccPSzF2D0");
+
+
+
+
             var ApiKey = "dict.1.1.20210609T093927Z.888d975cd57427e7.a81c833f63a959024fa47afb7603fd71eec4747f";
-            Console.WriteLine("Введите слово на русском");
-            var Word = Console.ReadLine();
-            var url = $"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={ApiKey}&lang=ru-ru&text={Word}";
 
-            var request = WebRequest.Create(url);
+           
 
-            var response = request.GetResponse();
-            var httpStatusCode = (response as HttpWebResponse).StatusCode;
-
-            if (httpStatusCode != HttpStatusCode.OK)
+            bot.OnMessage += (s, arg) =>
             {
-                Console.WriteLine(httpStatusCode);
-                return;
-            }
+                var Word = arg.Message.Text;
+                var url = $"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={ApiKey}&lang=ru-ru&text={Word}";
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {Word}");
+                var request = WebRequest.Create(url);
 
-            try
-            {
-                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                var response = request.GetResponse();
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode != HttpStatusCode.OK)
                 {
-                    string result = streamReader.ReadToEnd();
-                    var datas = JsonConvert.DeserializeObject<Root>(result);
-
-
-                    foreach (var syn in datas.def[0].tr)
-                    {
-                        Console.WriteLine(syn.text);
-
-                        if (syn.syn != null)
-                        {
-                            foreach (var s in syn.syn)
-                            {
-                                Console.WriteLine($"    {s.text}");
-                            }
-                        }   
-                    }
+                    Console.WriteLine(httpStatusCode);
+                    return;
                 }
-            }
 
-            catch
-            {
-                Console.WriteLine("Это слово некоректно, или не имеет синонимов");
-            }
+                try
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string result = streamReader.ReadToEnd();
+                        var datas = JsonConvert.DeserializeObject<Root>(result);
+                        result = null;
+
+                        foreach (var syn in datas.def[0].tr)
+                        {
+                            Console.WriteLine(syn.text);
+                            result += $"*{syn.text}*\n";
+
+                            if (syn.syn != null)
+                            {
+                                foreach (var M in syn.syn)
+                                {
+                                    result += $"    {M.text}" + Environment.NewLine ;
+                                }
+                            }
+                        }
+
+                        bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Результат по запросу слова: {Word}\n{result}", ParseMode.Markdown);
+                    }
+
+                }
+
+                catch
+                {
+                    Console.WriteLine("Это слово некоректно, или не имеет синонимов");
+                }
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
         }
     }
 }
